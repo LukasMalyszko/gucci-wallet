f2a6448543ddfde67f39c629c5d07d69c07b2812
Lukasz Malyszko <lukas.malyszko@gmail.com>
2023-04-01 19:03:53 +0200

    druga poprawna wersja

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -7,81 +7,68 @@ import "../styles/home-page.scss";
 
 // setupCounter(document.querySelector('#counter'))
 
-const img = document.getElementById("product-img");
+const imgDesktop = document.querySelector(".product-img-desktop");
+const imgMobile = document.querySelector(".product-img-mobile");
 const firstColor = document.querySelector(".first");
 const secondColor = document.querySelector(".second");
 const thirdColor = document.querySelector(".third");
 const fourthColor = document.querySelector(".fourth");
 const colors = document.querySelectorAll(".product-container-component__color");
-let colorName = document.getElementById("color-name");
 const addToCart = document.getElementById("addToCart");
+
+let selectedColor = "łososiowy";
 
 function handleColorClick(event) {
   const clickedElement = event.target;
   for (const color of colors) {
     color.setAttribute("data-select", color === clickedElement);
-  }
-
-  //window.addEventListener("resize", function () {
-    if (window.innerWidth > 600) {
-      changeBigImage();
-    } else {
-      changeSmallImage();
+    if (color === clickedElement) {
+      selectedColor = color.getAttribute("data-name");
     }
-  //});
-  changeColorName();
+    changeImgL();
+    changeImgS();
+  }
 }
 
 for (const color of colors) {
   color.addEventListener("click", handleColorClick);
 }
 
-function changeBigImage() {
+function changeImgL() {
   if (firstColor.getAttribute("data-select") === "true") {
-    img.src = "/images/Desktop, tablet 1.png";
+    imgDesktop.src = "/images/Desktop, tablet 1.png";
   } else if (secondColor.getAttribute("data-select") === "true") {
-    img.src = "/images/Desktop, tablet 2.png";
+    imgDesktop.src = "/images/Desktop, tablet 2.png";
   } else if (thirdColor.getAttribute("data-select") === "true") {
-    img.src = "/images/Desktop, tablet 3.png";
+    imgDesktop.src = "/images/Desktop, tablet 3.png";
   } else if (fourthColor.getAttribute("data-select") === "true") {
-    img.src = "/images/Desktop, tablet 4.png";
+    imgDesktop.src = "/images/Desktop, tablet 4.png";
   }
 }
 
-function changeSmallImage() {
+function changeImgS() {
   if (firstColor.getAttribute("data-select") === "true") {
-    img.src = "/images/Mobile 1.png";
+    imgMobile.src = "/images/Mobile 1.png";
   } else if (secondColor.getAttribute("data-select") === "true") {
-    img.src = "/images/Mobile 2.png";
+    imgMobile.src = "/images/Mobile 2.png";
   } else if (thirdColor.getAttribute("data-select") === "true") {
-    img.src = "/images/Mobile 3.png";
+    imgMobile.src = "/images/Mobile 3.png";
   } else if (fourthColor.getAttribute("data-select") === "true") {
-    img.src = "/images/Mobile 4.png";
+    imgMobile.src = "/images/Mobile 4.png";
   }
 }
 
-function changeColorName () {
-  if (firstColor.getAttribute("data-select") === "true") {
-    colorName.innerHTML = "Łososiowy";
-  } else if (secondColor.getAttribute("data-select") === "true") {
-    colorName.innerHTML = "Rózowy";
-  } else if (thirdColor.getAttribute("data-select") === "true") {
-    colorName.innerHTML = "Biszkoptowy";
-  } else if (fourthColor.getAttribute("data-select") === "true") {
-    colorName.innerHTML = "Szary";
-  }
-}
-
-function alert() {
+function alert(color) {
   const alert = document.createElement("div");
-  alert.classList.add("product-container-component__alert")
-  const container = document.querySelector(".product-container-component");
-  let color = colorName.innerHTML.toLowerCase();
-  
-  alert.textContent = `Portfel w kolorze ${color}m został dodany do koszyka`;
-  container.appendChild(alert);
+  alert.classList.add("product-container-component__alert");
+  const productContainer = document.querySelector(
+    ".product-container-component"
+  );
 
-  setTimeout(() => container.removeChild(alert), 2500);
+  alert.textContent = `Portfel w kolorze ${selectedColor}m został dodany do koszyka`;
+  productContainer.appendChild(alert);
+
+  setTimeout(() => productContainer.removeChild(alert), 2500);
 }
 
 addToCart.addEventListener("click", alert);
